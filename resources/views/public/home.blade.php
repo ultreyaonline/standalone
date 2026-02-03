e9414fd6d7e74f2751ee5b1061f614c1c1acfd78
Ultreya <ultreyaonline+github@gmail.com>
2026-02-03 16:34:57 -0500

    Improve layout and spacing

--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -9,18 +9,20 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-10 offset-md-1 col-xl-8 offset-xl-2">
+            <div class="col-6 offset-3">
                 <div class="text-center">
-                    <img src="logo/logo.jpg" title="{{ config('site.community_long_name') }}" class="text-center">
+                    <img src="logo/logo.jpg" title="{{ config('site.community_long_name') }}" class="text-center img-fluid">
                 </div>
+            </div>
+            <div class="col-md-10 offset-md-1 col-xl-8 offset-xl-2">
                 <div class="m-3">
                     <h1 class="text-center">Welcome to<br>{{ config('site.community_long_name') }}</h1>
                 </div>
                 <div class="m-3">
-                    <p style="font-size: 24px">Tres Dias is a three-day Christian renewal experience which aims to concentrate on the person and teachings of Jesus Christ.
-                        While Tres Dias explores the basic Christian beliefs, it is best described as a spiritual encounter with Christ.
-                        Many who have attended a weekend have experienced a deeper and more meaningful relationship with Christ as they sense His love in a dynamic way.
-                        Tres Dias is interdenominational in its teaching and outreach. It is a laity-led experience and has no affiliation with any particular Christian denomination.</p>
+                    <p style="font-size: 24px">Tres Dias is a three-day Christian renewal experience which aims to concentrate on the person and teachings of Jesus Christ.</p>
+                    <p style="font-size: 24px">While Tres Dias explores the basic Christian beliefs, it is best described as a spiritual encounter with Christ.</p>
+                    <p style="font-size: 24px">Many who have attended a weekend have experienced a deeper and more meaningful relationship with Christ as they sense His love in a dynamic way.</p>
+                    <p style="font-size: 24px">Tres Dias is interdenominational in its teaching and outreach. It is a laity-led experience and has no affiliation with any particular Christian denomination.</p>
                 </div>
                 <div class="text-center">
                     <a class="btn btn-lg btn-primary" href="{{ url('/about') }}">Learn More</a>
