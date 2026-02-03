e2d76e64a7c172b497b140c5e7b91f23533b8e4d
Ultreya <ultreyaonline+github@gmail.com>
2026-02-03 16:33:47 -0500

    Update wording

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -17,6 +17,6 @@ return [
     'sent' => 'We have emailed password instructions! PLEASE GO CHECK YOUR EMAIL: CLICK ON THE LINK IN THE EMAIL so you can set a new password.',
     'throttled' => 'Please wait before retrying.',
     'token' => 'This password reset token is invalid or has expired because it was created too long ago. If you need to set/reset your password, please request a new token by using the Forgotten Password page.',
-    'user' => "We can't find a user with that email address.",
+    'user' => "We can't find an authorized user with those credentials.",
 
 ];
