bd3fba3a38fd8318f50b9f1b1a04a1e8b07c3364
Ultreya <ultreyaonline+github@gmail.com>
2026-04-02 22:34:05 -0400

    Remove unused BroadcastServiceProvider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,7 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\AuthServiceProvider::class,
-    // App\Providers\BroadcastServiceProvider::class,
     App\Providers\EventServiceProvider::class,
 
     App\Providers\HorizonServiceProvider::class,
