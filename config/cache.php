348c9e504faf187af5dafe9a0b46018e5252a2d7
Ultreya <ultreyaonline+github@gmail.com>
2026-04-14 17:18:57 -0400

    Update Laravel skeleton to 13.2

--- config/cache.php
+++ config/cache.php
@@ -112,7 +112,7 @@ return [
     |
     */
 
-    'prefix' => env('CACHE_PREFIX', Str::slug((string) env('APP_NAME', 'laravel'), '_').'_cache_'),
+    'prefix' => env('CACHE_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-cache-'),
 
     /*
     |--------------------------------------------------------------------------
